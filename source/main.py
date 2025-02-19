3f9825be788ce351e130a2c340d6d8153f57c6da
alissonarsa <alissonarsa@gmail.com>
2025-02-18 22:21:49 -0300

    começando a deixar funcional o menu

--- source/main.py
+++ source/main.py
@@ -7,12 +7,20 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from lib.funcoes import *
 
 while True:
-    print("""
+    menu = input(f"""
     
     1 - Novo Chamado
     2 - Buscar Chamado
     3 - Listar Chamados por Prioridade
-    3 - Reverter Chamado
+    4 - Reverter Chamado
     5 - Limpar lista de Chamados
     
-    """)
+    Escolha: """)
+    
+    if menu == "1":
+        print("Vamos abrir seu CHAMADO")
+        descricao = input("Descrição: ").lower()
+        print("Defina a prioridades: BAIXA, MÉDIA, ALTA")
+        prioridade = input("Escolha: ").upper()
+        status = "ABERTO"
+        gerar_chamado(descricao, prioridade, status)

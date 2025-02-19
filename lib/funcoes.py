05fa10d6ffb2e78dc45de5c458ab0614d1825d82
alissonarsa <alissonarsa@gmail.com>
2025-02-18 21:44:27 -0300

    Separado as funções em grupo (depois testar em class), criado as funções de chamados e reformulado a função de gerar chamados com as novas funções

--- lib/funcoes.py
+++ lib/funcoes.py
@@ -6,11 +6,14 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Construir o caminho para o arquivo JSON relativo ao diretório base
 PATH = os.path.join(BASE_DIR, "../database/chamados.json")
 
+# funções gerais
 
 def inicializar_arquivo():
     if not os.path.exists(PATH) or os.path.getsize(PATH) == 0:
         with open(PATH, 'w') as file:
             json.dump({"contador": 0, "chamados": []}, file, indent=4)
+
+# funções para o contador
 
 def carregar_contador():
     inicializar_arquivo()
@@ -26,7 +29,34 @@ def salvar_contador(valor):
     with open(PATH, "w") as f:
         json.dump(data, f, indent=4)
 
-def gerar_chamado():
+# funções para os chamados
+
+def carregar_chamados():
+    inicializar_arquivo()
+    with open(PATH, "r") as f:
+        data = json.load(f)
+        return data.get("chamados", [])
+
+def salvar_chamados(chamados):
+    inicializar_arquivo()
+    with open(PATH, "r") as f:
+        data = json.load(f)
+    data["chamados"] = chamados
+    with open(PATH, "w") as f:
+        json.dump(data, f, indent=4)
+
+# funções para gerar chamados
+
+def gerar_chamado(descricao, prioridade, status):
     contador = carregar_contador() + 1
     salvar_contador(contador)
-    return f"#{contador:09d}"
+    chamado = {
+        "id": f"#{contador:09d}",
+        "descricao": descricao,
+        "prioridade": prioridade,
+        "status": status
+    }
+    chamados = carregar_chamados()
+    chamados.append(chamado)
+    salvar_chamados(chamados)
+    return chamado
--- tests/testes.py
+++ tests/testes.py
@@ -7,10 +7,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from lib.funcoes import *
 
 if __name__ == "__main__":
-    # Testar a criação de um novo chamado
-    chamado = gerar_chamado()
-    print("Novo chamado criado:", chamado)
-    
     # Testar a sequência
     sequencia = carregar_contador()
     print("Sequência atual:",sequencia)
+
+    # Testar a listagem de chamados
+    chamados = carregar_chamados()
+    print("Lista de chamados:", chamados)
+
+    # Testar a criação de um novo chamado
+    chamado = gerar_chamado("Exemplo de descrição", "alta", "aberto")
+    print("Novo chamado criado:", chamado)
